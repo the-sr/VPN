c40a6c963588a4fce45321e15b2bf262fab749b0
the-sr <thesagarrijal@gmail.com>
2025-09-07 15:51:22 +0545

    feat: config for client and server connection

--- Client/src/main/java/com/Client.java
+++ Client/src/main/java/com/Client.java
@@ -1,9 +1,18 @@
 package com;
 
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
+        try {
+
+            Socket socket = new Socket("127.0.0.1", 5000);
+            System.out.println("Connected successfully");
+
+        } catch (Exception e) {
+            System.out.println("Error in socket connection: " + e.getMessage());
+        }
 
     }
 }
--- Server/src/main/java/com/Server.java
+++ Server/src/main/java/com/Server.java
@@ -1,9 +1,21 @@
 package com;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
+        try {
+
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Socket Server stared, listening at port 5000, waiting for client...");
+            Socket socket = serverSocket.accept();
+            System.out.println("Client successfully connected: " + socket.getInetAddress());
+
+        } catch (Exception e) {
+            System.out.println("Error in server socket: " + e.getMessage());
+        }
 
     }
 }
